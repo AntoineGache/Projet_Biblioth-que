4604f5a469c645163843be76f31212ac4085d46d
Antoine Gache <73948752+AntoineGache@users.noreply.github.com>
2021-06-09 14:10:15 +0200

    Identifiants

--- Projet Bibliotheque/src/Modele/Jdbc.java
+++ Projet Bibliotheque/src/Modele/Jdbc.java
@@ -8,10 +8,10 @@ public class Jdbc {
 	public Jdbc() throws SQLException, ClassNotFoundException {
 		//Connexion
 	  	DriverManager.registerDriver(new oracle.jdbc.driver.OracleDriver());
-		System.out.println("Connexion à la base de données de la bibliothèque...");
-		//statement = DriverManager.getConnection ("jdbc:oracle:thin:@madere:1521:info","apellegr", "azerty").createStatement ();
-		chStatement = DriverManager.getConnection ("jdbc:oracle:thin:@localhost:1521:info","apellegr", "azerty").createStatement ();
-		System.out.println("Connecté à la base de données de la bibliothèque...");
+		System.out.println("Connexion Ã  la base de donnÃ©es de la bibliothÃ¨que...");
+		//statement = DriverManager.getConnection (identifiants de connexion).createStatement ();
+		chStatement = DriverManager.getConnection (identifiants de connexion).createStatement ();
+		System.out.println("ConnectÃ© Ã  la base de donnÃ©es de la bibliothÃ¨que...");
 	}
 
 	 public Statement getStatement() {
